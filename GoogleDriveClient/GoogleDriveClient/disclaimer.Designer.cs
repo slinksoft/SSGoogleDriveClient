23507e6c81d54886af2b60fb03278d98ec20f5b6
slinksoft <slinkpro@gmail.com>
2020-01-13 22:33:58 -0500

    version 1.2 revisions and additions

---
+++ GoogleDriveClient/GoogleDriveClient/disclaimer.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace GoogleDriveClient
+{
+    partial class disclaimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(disclaimer));
+            this.message = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.aButton = new System.Windows.Forms.Button();
+            this.dButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message.Location = new System.Drawing.Point(12, 84);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(511, 220);
+            this.message.TabIndex = 0;
+            this.message.Text = resources.GetString("message.Text");
+            this.message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DISCLAIMER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(204, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "As of version 1.2:";
+            // 
+            // aButton
+            // 
+            this.aButton.Location = new System.Drawing.Point(113, 327);
+            this.aButton.Name = "aButton";
+            this.aButton.Size = new System.Drawing.Size(75, 23);
+            this.aButton.TabIndex = 3;
+            this.aButton.Text = "Accept";
+            this.aButton.UseVisualStyleBackColor = true;
+            this.aButton.Click += new System.EventHandler(this.AButton_Click);
+            // 
+            // dButton
+            // 
+            this.dButton.Location = new System.Drawing.Point(335, 327);
+            this.dButton.Name = "dButton";
+            this.dButton.Size = new System.Drawing.Size(75, 23);
+            this.dButton.TabIndex = 4;
+            this.dButton.Text = "Decline";
+            this.dButton.UseVisualStyleBackColor = true;
+            this.dButton.Click += new System.EventHandler(this.DButton_Click);
+            // 
+            // disclaimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 356);
+            this.Controls.Add(this.dButton);
+            this.Controls.Add(this.aButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.message);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(544, 395);
+            this.MinimumSize = new System.Drawing.Size(544, 395);
+            this.Name = "disclaimer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Disclaimer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button aButton;
+        private System.Windows.Forms.Button dButton;
+    }
+}
---
+++ GoogleDriveClient/GoogleDriveClient/disclaimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleDriveClient
+{
+    public partial class disclaimer : Form
+    {
+        public disclaimer()
+        {
+            InitializeComponent();
+        }
+
+        private void AButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The client will automatically check for updates at SlinkSoft on every startup. Updates for the client are necessary due to" +
+                " changes in code on Google's side. We highly suggest updating when an update is available to maintain 100% functionality of the client."
+                , "Regarding Updates");
+            Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/SSGoogleDriveClient");
+            File.Create(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/SSGoogleDriveClient/accounts.ss").Close();
+            Application.Restart();
+        }
+
+        private void DButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
